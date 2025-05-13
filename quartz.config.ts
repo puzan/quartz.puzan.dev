f805482d77fd5cdb6ff16b7e71d3232ae6836c92
Ilya Zonov <izonov@gmail.com>
2025-05-13 23:16:29 +0300

    fix: Remove favicon plugin

--- quartz.config.ts
+++ quartz.config.ts
@@ -96,7 +96,6 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
